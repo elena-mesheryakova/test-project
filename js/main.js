63e5cda768698b7d61f40d7ad566fd53eb408151
Elena <mesheryakova-elena90@yandex.ru>
2021-04-27 18:12:57 +0300

    added new project

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(function(){
+	$(document).ready(function() {
+        
+		$('.header__menu-burger').click(function() {
+			$('.header__menu-burger').toggleClass('open-menu');
+			$('.header__menu').toggleClass('open-menu');
+		});
+	});
+});
